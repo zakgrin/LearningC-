48569b567d7855fb3a7b1eaf03f0077c84b26edb
zakgrin <zakariya.abugrin@gmail.com>
2020-10-13 06:46:21 +0200

    added bool operations

--- 3_arthematic_operations.cpp
+++ 3_arthematic_operations.cpp
@@ -6,6 +6,7 @@ int main(){
     double apple = 40;
     double orange = 30;
 
+    // Basic Operations:
     int sum = apple + orange;
     int dif = apple - orange;
     int mul = apple * orange;
@@ -17,6 +18,23 @@ int main(){
     cout << "mul = " << mul << endl;
     cout << "integer div = " << i_div << endl;
     cout << "double div = " << d_div << endl;
+
+    // Special Form: 
+    int j = 1; 
+    j++; // to add 1
+    ++j; // to add 1
+    j += 1; // to add any number
+    j -= 1; // to substract any number
+    j *= 2; // to multiply by any number
+    j /= 2; // to devide by any number
+    cout << "j = " << j << endl;
+
+    // Understanding Operations:
+    int c = 1;
+    cout << "c=" << c << endl;
+    cout << "c++=" << c++ << endl; // addition after line
+    cout << "c=" << c << endl; // here we see value of 2
+    cout << "++c=" << ++c << endl; // addition during the line, here we see value of 3
 
     return 0;
 }
---
+++ 5_bool_operations.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // Define Variables:
+    int x = 10;
+    int y = 10;
+    int z = 30;
+
+    // Binary Bool Operations;
+    bool equal = x == y;
+    bool notEqual = x != y;
+    bool greater = x > y;
+    bool less = x < y;
+    bool greaterOrEqual = x >= y;
+    bool lessOrEqual = x <= y;
+
+    cout << "x = " << x << ", y = " << y << endl;
+    cout << "x == y: " << equal << endl;
+    cout << "x != y: " << notEqual << endl;
+    cout << "x > y: " << greater << endl;
+    cout << "x < y: " << less << endl;
+    cout << "x >= y: " << greaterOrEqual << endl;
+    cout << "x <= y: " << lessOrEqual << endl;
+
+    // Multiple Bool Operations: 
+    // cout << "x == y == z: " << x == y == z << endl; // not allowed to compare 3 variables directly
+    cout << "(x == y) and (x == z): " << ((x == y) && (x == z)) << endl; // we must include outer ()
+    cout << "(x == y) or (x == z): " << ((x == y) || (x == z)) << endl;
+
+    return 0;
+}
