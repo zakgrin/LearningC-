f913dd4cec20010d39d39b3b0ebad7c604937d53
zakgrin <zakariya.abugrin@gmail.com>
2020-10-14 00:16:34 +0200

    updated while loop

--- 11_while_loop.cpp
+++ 11_while_loop.cpp
@@ -3,12 +3,28 @@ using namespace std;
 
 int main(){
 
-    int counter = 0;
-    
-    while (counter < 1000){
-        cout << counter;
+    // 1. Range Loop:
+    int counter = 1;
+    while (counter <= 10){
+        cout << counter << " ";
         counter++;    
     }
-    
+
+    // 2. Infinite Loop:
+        /* There are two ways to control loops:
+        - break: to exit the entire loop
+        - continue: to continue to next loop without reading the code below
+                    (ignore the rest of the code)
+        */
+    int n = 0;
+    while (true){
+        n++;
+        if (n == 5)
+            continue;
+        cout << n;
+        if (n == 8)
+            break; // exit condition.
+    }
+
     return 0;
 }
