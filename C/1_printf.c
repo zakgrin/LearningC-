38415f319429c3d114852a3810e55a8e53db350a
zakgrin <zakariya.abugrin@gmail.com>
2020-10-14 19:47:45 +0200

    add print statement

--- C/1_printf.c
+++ C/1_printf.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 
 int main(){
-
-    printf("Hello World !");
+    /*
+    Notes in c and c++ are the same.
+    */
+    // Print: note here "\n" is used to start a new line.
+    printf("Hello! \n");
+    printf("Zakariya");
     return 0;
 }
