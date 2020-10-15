1cfc0ad877deca090413974e69a1a8070773a9d9
zakgrin <zakariya.abugrin@gmail.com>
2020-10-15 10:09:28 +0200

    finish print for both c and c++

--- C/1_printf.c
+++ C/1_printf.c
@@ -6,7 +6,7 @@ int main(){
     Notes in c and c++ are the same.
     */
     // Print: note here "\n" is used to start a new line.
-    printf("Hello! \n");
-    printf("Zakariya");
+    printf("Hello\nWorld:\t C\n");
+    printf("This is the 2nd line");
     return 0;
 }
--- Cpp/1_cout.cpp
+++ Cpp/1_cout.cpp
@@ -6,7 +6,7 @@ Multiple lines note:
 */
 
 int main(){
-    cout << "Hello\nWrold:\tZakariya" << endl
+    cout << "Hello\nWrold:\tC++" << endl
          << "This is the 2nd line";
     return 0;
 }
